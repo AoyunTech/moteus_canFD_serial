74682063850a0e9477122b8ac727ad4ecd21187b
AoyunTech <aoyuntech@163.com>
2021-06-22 18:05:35 +0800

    update example.py

--- example.py
+++ example.py
@@ -6,13 +6,13 @@ from kinematics import Kinematics
 
 
 def main():
-    controller_knee = Controller(controller_ID = 1)
-    controller_hip = Controller(controller_ID = 2)
-    response_data_c1 = controller_knee.get_data()
-    robot_knee_rot = response_data_c1[MoteusReg.MOTEUS_REG_POSITION]
-    response_data_c2 = controller_hip.get_data()
-    robot_hip_rot = response_data_c2[MoteusReg.MOTEUS_REG_POSITION]
-    kinematics = Kinematics(robot_knee_rot, robot_hip_rot)
+    controller_shank = Controller(controller_ID = 8)
+    controller_thigh = Controller(controller_ID = 7)
+    response_data_c1 = controller_shank.get_data()
+    robot_shank_rot = response_data_c1[MoteusReg.MOTEUS_REG_POSITION]
+    response_data_c2 = controller_thigh.get_data()
+    robot_thigh_rot = response_data_c2[MoteusReg.MOTEUS_REG_POSITION]
+    kinematics = Kinematics(robot_shank_rot, robot_thigh_rot)
 
     freq=300
 
@@ -24,21 +24,18 @@ def main():
         x = -40+155*math.cos(phase)
 
         if kinematics.if_ik_possible(x, z):
-            robot_hip_rot_calculated, robot_knee_rot_calculated = kinematics.ik(x, z)
+            robot_thigh_rot_calculated, robot_shank_rot_calculated = kinematics.ik(x, z)
 
-            response_data_c1 = controller_knee.get_data()
-            robot_knee_rot_measured = response_data_c1[MoteusReg.MOTEUS_REG_POSITION]
-            response_data_c2 = controller_hip.get_data()
-            robot_hip_rot_measured = response_data_c2[MoteusReg.MOTEUS_REG_POSITION]
+            response_data_c1 = controller_shank.get_data()
+            robot_shank_rot_measured = response_data_c1[MoteusReg.MOTEUS_REG_POSITION]
+            response_data_c2 = controller_thigh.get_data()
+            robot_thigh_rot_measured = response_data_c2[MoteusReg.MOTEUS_REG_POSITION]
 
-
-
-
-            print(f'pos c1: {robot_hip_rot_measured*360/6:.2f} pos c2: {robot_knee_rot_measured*360/6:.2f}')
+            print(f'pos c1: {robot_thigh_rot_measured*360/6:.2f} pos c2: {robot_shank_rot_measured*360/6:.2f}')
 
 
-            controller_hip.set_position(position=robot_hip_rot_calculated, max_torque=1, kd_scale=0.5, kp_scale=1)
-            controller_knee.set_position(position=robot_knee_rot_calculated, max_torque=1, kd_scale=0.5, kp_scale=1)
+            controller_thigh.set_position(position=robot_thigh_rot_calculated, max_torque=1, kd_scale=0.5, kp_scale=1)
+            controller_shank.set_position(position=robot_shank_rot_calculated, max_torque=1, kd_scale=0.5, kp_scale=1)
 
         # response_data_c1 = controller_knee.get_data()
         # robot_knee_rot_org = response_data_c1[MoteusReg.MOTEUS_REG_POSITION]
@@ -58,12 +55,10 @@ def main():
         # print(f'knee: {knee_deg_org:.2f} / {knee_deg_processed:.2f}, hip: {hip_deg_org:.2f} / {hip_deg_processed:.2f}, X: {x:.2f}, Z: {z:.2f}')
 
 
-
         sleep = (1/freq)-(time.time()-freq_measure_time)
         if sleep < 0: sleep = 0
         time.sleep(sleep)
         #print(f'freq: {1 / (time.time() - freq_measure_time):.1f}')
-
 
 if __name__ == '__main__':
     main()
